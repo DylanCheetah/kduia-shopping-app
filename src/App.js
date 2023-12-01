a95e28a8be0cd61e8362b22c53f6741e8d6b9363
DylanCheetah <dylan_the_cheetah@outlook.com>
2023-12-01 14:34:53 -0500

    Finished shopping app.

--- src/App.js
+++ src/App.js
@@ -8,8 +8,30 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppProvider>
+        <div className='container'>
+            <div className="row">
+                <div className="col">
+                    <CartValue />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <ExpenseList />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <ItemSelected />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <Location />
+                </div>
+            </div>
+        </div>
+    </AppProvider>
   );
 }
 
